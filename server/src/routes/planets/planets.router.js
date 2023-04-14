2bc2d5905332a5ba4331d409069540e2793d5a15
Madina <madinatussupova0820@gmail.com>
2023-04-14 10:24:47 +0600

    get /planets; add cors

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const planetsRouter = express.Router();
+const { getAllPlanets } = require('./planets.controller');
 
-planetsRouter.get('/planets', (req, res) => {
-	
-})
+planetsRouter.get('/planets', getAllPlanets);
+
+module.exports = planetsRouter;
