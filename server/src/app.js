cbd19cd6725a5a512d240b8f294286c4b7154e6d
Madina <madinatussupova0820@gmail.com>
2023-04-21 10:01:15 +0600

    build react js front end to serve

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
@@ -9,6 +10,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
