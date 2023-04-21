35c708f9a513a8c18015a2325de879d40f490f9b
Madina <madinatussupova0820@gmail.com>
2023-04-21 10:29:27 +0600

    add morgan logger middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 app.use(cors({
 	origin: ['http://localhost:3000', 'http://localhost:3000/']
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
