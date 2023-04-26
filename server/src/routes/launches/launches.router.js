e8993d46d04fb3b60b55f65a3647f5b673cb0748
Madina <madinatussupova0820@gmail.com>
2023-04-26 10:05:04 +0600

    build a data access layer for launches

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { getAllLaunches } = require('../launches/launches.controller');
+const { httpGetAllLaunches } = require('../launches/launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', getAllLaunches);
+launchesRouter.get('/launches', httpGetAllLaunches);
 
 module.exports = launchesRouter;
