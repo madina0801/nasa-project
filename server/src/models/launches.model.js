ab654edaa2f008b3f8a8ca81a3720bb603723be3
Madina <madinatussupova0820@gmail.com>
2023-04-26 10:39:52 +0600

    addNewLaunch function

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,14 +1,16 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
-	flightNumber: 100,
+	// flightNumber: 100,
 	mission: 'Mission name 1',
 	rocket: 'Explorer IS1',
 	launchDate: new Date('December 27, 2030'),
 	destination: 'Kepler-442 b',
-	customers: ['ZTM', 'NASA'],
-	upcoming: true,
-	success: true,
+	// customers: ['ZTM', 'NASA'],
+	// upcoming: true,
+	// success: true,
 };
 
 launches.set(launch.flightNumber, launch);
@@ -17,6 +19,19 @@ function getAllLaunches() {
 	return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+	latestFlightNumber++;
+	launches.set(
+		latestFlightNumber,
+		Object.assign(launch, {
+			flightNumber: latestFlightNumber,
+			customers: ['ZTM', 'NASA'],
+			upcoming: true,
+			success: true,
+		}));
+}
+
 module.exports = {
 	getAllLaunches,
+	addNewLaunch,
 }
