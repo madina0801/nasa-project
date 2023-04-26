e107a16282f82add8e14b035f05b951110d0708b
Madina <madinatussupova0820@gmail.com>
2023-04-26 13:02:26 +0600

    add post route to launches

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { httpGetAllLaunches } = require('../launches/launches.controller');
+const { httpGetAllLaunches, httpAddNewLaunch } = require('../launches/launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
 
 module.exports = launchesRouter;
