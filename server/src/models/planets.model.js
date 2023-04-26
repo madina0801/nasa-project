886f5fa26b45b9a936c6efde5da0e038fac0c19e
Madina <madinatussupova0820@gmail.com>
2023-04-26 10:11:45 +0600

    build data access layer for planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -32,7 +32,11 @@ function loadPlanetsData() {
 	});
 }
 
+function getAllPlanets() {
+	return habitablePlanets;
+}
+
 module.exports = {
 	loadPlanetsData,
-	planets: habitablePlanets,
+	getAllPlanets
 }
