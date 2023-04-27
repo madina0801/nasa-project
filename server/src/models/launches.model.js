058f280154a350ade7f2d1d8f230d48420876bd2
Madina <madinatussupova0820@gmail.com>
2023-04-27 14:14:36 +0600

    connect post /launches with front end dashboard

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
 	mission: 'Mission name 1',
 	rocket: 'Explorer IS1',
 	launchDate: new Date('December 27, 2030'),
-	destination: 'Kepler-442 b',
+	target: 'Kepler-442 b',
 	// customers: ['ZTM', 'NASA'],
 	// upcoming: true,
 	// success: true,
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -9,7 +9,7 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
 	const launch = req.body;
-	if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+	if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
 		return res.status(400).json({
 			error: 'Missing required launch property',
 		});
